d57c0bfe72e671dacb8544b449b619cab0b860b6
sehaj chawla <sehaj.chawla@softobiz.com>
2022-11-01 12:18:11 +0530

    deploy

--- routes/index.js
+++ routes/index.js
@@ -1,40 +1,39 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-const messages=[
+const messages = [
   {
-    text:"hi dear",
-    user:"sehaj",
-    added:new Date()
+    text: "hi dear",
+    user: "sehaj",
+    added: new Date(),
   },
   {
-    text:"hi",
-    user:"rahul",
-    added:new Date()
+    text: "hi",
+    user: "rahul",
+    added: new Date(),
   },
   {
-    text:"hi dear",
-    user:"raghav",
-    added:new Date()
+    text: "hi dear",
+    user: "raghav",
+    added: new Date(),
   },
   {
-    text:"heloo",
-    user:"guri",
-    added:new Date()
-  }
-]
+    text: "hello",
+    user: "guri",
+    added: new Date(),
+  },
+];
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: "Mini Messageboard", messages: messages });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Mini Messageboard", messages: messages });
 });
-router.get('/new',function(req,res){
-  res.render('form',{ title: "NewMessage"})
-})
-router.post('/new',function(req,res){
- let user=req.body.user
- let message=req.body.message
- messages.push({text:message,user:user,added:new Date()})
- res.redirect('/')
-})
-
+router.get("/new", function (req, res) {
+  res.render("form", { title: "NewMessage" });
+});
+router.post("/new", function (req, res) {
+  let user = req.body.user;
+  let message = req.body.message;
+  messages.push({ text: message, user: user, added: new Date() });
+  res.redirect("/");
+});
 
 module.exports = router;
